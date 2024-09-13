95fef2eb03f67c896f78d6c97555f07ebe90019b
Yabe12 <yeabsirabehailu92@gmail.com>
2024-09-14 00:29:19 +0300

    change something

---
+++ Front/src/App.jsx
@@ -0,0 +1,10 @@
+// App.jsx
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App; // Make sure this line is exporting the default component
---
+++ Front/src/main.jsx
@@ -0,0 +1,6 @@
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'; // Correctly importing the default export
+
+ReactDOM.render(<App />, document.getElementById('root'));
